5afeb8a6dffe23c132d125a21a3349e16734dcd6
Eduardo Almeida <emmet@Eduardos-MacBook-Pro.local>
2019-10-08 18:42:38 -0300

    install zsh

--- play.sh
+++ play.sh
@@ -2,6 +2,10 @@
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew install zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 brew cask install google-chrome
 
 brew cask install iterm2
